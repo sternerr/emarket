0ff37348c7aabc7772d21a3690f339983ac63842
Noah <volpy2000@gmail.com>
2025-08-27 17:34:12 +0200

    feat(client): express setyp

---
+++ server/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+class App {
+	#express
+
+	constructor() {
+		this.#express = express();
+		this.middlewares();
+		this.routes();
+	}
+
+	middlewares() {
+		this.#express.use(express.json());
+		this.#express.use(express.urlencoded({ extended: true }));
+	}
+
+	routes() {
+		this.#express.get("/", (_, res) => {
+			res.json({ message: "Welcome to e-market backend" });
+		});
+	}
+
+	errorHandler() {
+		this.#express.use((err, _, res) => {
+			console.error(err.stack);
+			res.status(500).json({ error: "internal server error" });
+		});
+	}
+
+	listen(port) {
+		this.#express.listen(port, () => {
+			console.log(`Ecommerce backend @ http://localhost:${port}`);
+		})
+	}
+}
+
+export default new App();
